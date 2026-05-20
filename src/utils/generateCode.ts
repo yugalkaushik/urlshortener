bd5711784af61d3224f80a7beb019abe1adc6285
yugalkaushik <yugalkaushik14@gmail.com>
2026-05-20 17:04:59 +0530

    clear

--- src/utils/generateCode.ts
+++
@@ -1,8 +0,0 @@
-import { customAlphabet } from "nanoid";
-
-const alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-const nanoid = customAlphabet(alphabet, 7);
-
-export function generateShortCode(): string {
-  return nanoid();
-}
--- src/utils/validateUrl.ts
+++
@@ -1,8 +0,0 @@
-export function isValidUrl(url: string): boolean {
-  try {
-    const parsed = new URL(url);
-    return parsed.protocol === "http:" || parsed.protocol === "https:";
-  } catch {
-    return false;
-  }
-}

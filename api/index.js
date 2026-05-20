cfdd67b94a78ef4520368639cb0b7d516fe7b868
yugalkaushik <yugalkaushik14@gmail.com>
2026-05-20 16:39:59 +0530

    fix(api): ensure DB connects before serverless handler runs

--- api/index.js
+++ api/index.js
@@ -1,3 +1,22 @@
 const serverless = require('serverless-http');
 const app = require('../dist/app').default || require('../dist/app');
-module.exports = serverless(app);
+const { connectDB } = require('../dist/config/db');
+
+const handler = serverless(app);
+
+let dbReady = false;
+
+module.exports = async (req, res) => {
+	if (!dbReady) {
+		try {
+			await connectDB();
+			dbReady = true;
+		} catch (err) {
+			console.error('DB connection error in serverless handler:', err);
+			res.statusCode = 500;
+			return res.end('Database connection error');
+		}
+	}
+
+	return handler(req, res);
+};
